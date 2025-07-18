1aae3ed1b88ef144a50b67cf2aeeaa78ce135f43
qiaochao <qiaochao@icourt.cc>
2025-07-18 13:17:51 +0800

    feat: generate block

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"time"
 )
 
 //block struct
 type Block struct{
 	Index int
-	TimeStamp string
+	Timestamp string
 	// BPM : (Beats Per Minute)
 	BPM int 
 	Hash string //1）to save space， 2） Preserve integrity of the blockchain
@@ -18,7 +19,7 @@ type Block struct{
 
 //generate hash
 func calculateHash(block Block) string{
-	record := fmt.Sprint(block.Index) + block.TimeStamp + fmt.Sprint(block.BPM) + block.PrevHash
+	record := fmt.Sprint(block.Index) + block.Timestamp + fmt.Sprint(block.BPM) + block.PrevHash
 	h := sha256.New()
 	h.Write([]byte(record))
 	// Sum方法计算并返回SHA-256哈希值，nil参数表示将结果存储在新的字节切片中
@@ -27,7 +28,17 @@ func calculateHash(block Block) string{
 	return hex.EncodeToString(hashed)
 }
 
-
+//generate block 
+func generateBlock(oldBlock Block, BPM int) (Block,error){
+	var newBlock Block
+    t:= time.Now()
+	newBlock.Index = oldBlock.Index + 1 //index +1
+	newBlock.Timestamp = t.String() 
+	newBlock.BPM = BPM 
+	newBlock.PrevHash = oldBlock.Hash//current preHash = old block hash
+	newBlock.Hash = calculateHash(newBlock) //cal new hash
+	return newBlock, nil 
+}
 
 
 
